a008fc53ea312cc2bfca752a60620ec2224de8e7
Omor Faruk <mayemislam24@gmail.com>
2026-05-27 18:37:16 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    chatId: String,
-    number: String,
-    number_id: String,
-    country: String,
-    otpReceived: { type: Boolean, default: false }
-});
+const UserSchema =
+    new mongoose.Schema({
 
-module.exports = mongoose.model("User", UserSchema);
+        chatId: String,
+
+        number: String,
+
+        number_id: String,
+
+        country: String,
+
+        otpReceived: {
+            type: Boolean,
+            default: false
+        }
+
+    });
+
+module.exports =
+    mongoose.model(
+        "User",
+        UserSchema
+    );
