e41b81a4098a1d6bf018a850494a4bb2a8922dc1
Omor Faruk <mayemislam24@gmail.com>
2026-05-27 18:21:38 +0600

    Create Country.js

---
+++ models/Country.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const CountrySchema = new mongoose.Schema({
+    name: String,
+    range: String
+});
+
+module.exports = mongoose.model("Country", CountrySchema);
